6f62d5b28b75802c2f9021006a7ef9b5744135bd
salicrulucas <salicrulucas487@gmail.com>
2023-06-22 14:56:26 -0300

    index

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Super Heroes</title>
+</head>
+<body>
+  <h2>Superman</h2>
+  <h2>Linterna Verde</h2>
+  <h2>Flash</h2>
+  <h2>Batman</h2>
+  <h2>Mujer maravilla</h2>
+  <h2>Cyborg</h2>
+</body>
+</html>
